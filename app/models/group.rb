45eb81b3ed218e6cb680190ad909352cc7ca5bfd
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-05 13:49:32 -0500

    Add group_operation model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_and_belongs_to_many :operations
+  has_many :group_operations, dependent: :delete_all
+  has_many :groups, through: :group_operations
 
   validates :name, presence: true
   validates :icon, presence: true
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,6 +1,7 @@
 class Operation < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-  has_and_belongs_to_many :groups
+  has_many :group_operations, dependent: :delete_all
+  has_many :operations, through: :group_operations
 
   validates :name, presence: true
   validates :amount, presence: true
