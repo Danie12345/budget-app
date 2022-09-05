2861eed1f503902e0df7af0a74ad941f8a22ca0f
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-05 16:57:01 -0500

    Add user model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { "MyString" }
+    email { "user@example.com" }
+    password { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    before do 
+      @user = create(:user)
+    end
+
+    describe 'is valid' do
+      it 'when name is present' do
+        expect(@user).to be_valid
+      end
+
+      it 'when email is present' do
+        expect(@user).to be_valid
+      end
+
+      it 'when password is present' do
+        expect(@user).to be_valid
+      end
+    end
+
+    describe 'nil name' do
+      it 'is not valid' do
+        @user.name = nil
+        expect(@user).to_not be_valid
+      end
+    end
+    
+    describe 'nil email' do
+      it 'is not valid' do
+        @user.name = nil
+        expect(@user).to_not be_valid
+      end
+    end
+    
+    describe 'nil password' do
+      it 'is not valid' do
+        @user.name = nil
+        expect(@user).to_not be_valid
+      end
+    end
+  end
 end
