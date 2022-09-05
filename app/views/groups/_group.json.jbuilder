e65dc0fdb4759529b2dba6d112e28878811fc916
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-05 11:44:14 -0500

    Groups scaffolding

---
+++ app/views/groups/_group.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! group, :id, :name, :icon, :createdat, :user_id, :created_at, :updated_at
+json.url group_url(group, format: :json)
---
+++ spec/factories/groups.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :group do
+    name { "MyString" }
+    icon { "MyString" }
+    createdat { "2022-09-05" }
+    user { nil }
+  end
+end
