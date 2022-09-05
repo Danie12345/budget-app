36170a92a5c31783931d13851ee29475dafbfb4e
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-05 13:36:49 -0500

    Update group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,7 @@
 class Group < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_and_belongs_to_many :operations
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
