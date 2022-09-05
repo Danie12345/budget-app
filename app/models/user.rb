6ab7916c8923f76ddd80e4c45991dcade2eff7dc
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-05 13:27:52 -0500

    Update user model
    Add confirmable
    Add has many associations
    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+  has_many :groups, dependent: :delete_all, foreign_key: 'user_id'
+  has_many :operations, dependent: :delete_all, foreign_key: 'author_id'
+
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
 end
