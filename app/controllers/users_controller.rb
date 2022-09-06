ba978a5d25606de039a7cdfcc35a48ea920d3198
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-06 13:54:14 -0500

    Remove unneeded user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
-  # GET /users or /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
-  def show; end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit; end
-
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
