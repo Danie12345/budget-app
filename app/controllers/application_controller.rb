b318d3d1c64f415ab767c3c8e002293843a906f7
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-07 13:55:18 -0500

    Fix user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,25 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   def require_user
     unless current_user
       redirect_to get_started_path
     end
   end
-
+  
   private
   
   # Set client
   def set_client
     @client = current_user
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    # Permit the `subscribe_newsletter` parameter along with the other
+    # sign up parameters.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,4 +1,5 @@
 class OperationsController < ApplicationController
+  before_action :authenticate_user!
   before_action :require_user
   before_action :set_client
 
